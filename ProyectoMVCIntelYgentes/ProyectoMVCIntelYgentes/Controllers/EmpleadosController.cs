e3c853c8fdac2ca0c81c8a6ed34e1a9b310cfb44
Javier Murguia <cjmurguia@gmail.com>
2021-11-05 11:19:07 -0400

    cambiado verificar model state

--- ProyectoMVCIntelYgentes/ProyectoMVCIntelYgentes/Controllers/EmpleadosController.cs
+++ ProyectoMVCIntelYgentes/ProyectoMVCIntelYgentes/Controllers/EmpleadosController.cs
@@ -126,31 +126,28 @@ namespace ProyectoMVCIntelYgentes.Controllers
             }
             //Seccion para actualizar las otras tablas para el despido
             rrhhContext db = new rrhhContext();
-            if (ModelState.IsValid)
+            try
             {
-                try
-                {
-                    Empleado newEmpleado = db.Empleado.SingleOrDefault(b => b.EmpNo == empleado.EmpNo); ;
-                    newEmpleado.FechaContrato = null;
-                    DeptoEmp deptoEmpToUpdate = db.DeptoEmp.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
-                    deptoEmpToUpdate.FechaFin = DateTime.Now;
-                    Sueldos sueldo = db.Sueldos.SingleOrDefault(b => b.EmpNo == empleado.EmpNo);
-                    sueldo.HastaFecha = DateTime.Now;
-                    Cargo cargo = db.Cargo.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
-                    cargo.FechaFin = DateTime.Now;
-                    if (db.DeptoJefe.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo) != null)
-                    {
-                        DeptoJefe deptoJefe = db.DeptoJefe.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
-                        deptoJefe.FechaFin = DateTime.Now;
-                    }
-                    db.SaveChanges();
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("No pude escribir algo a la bd");
-                    return NotFound();
-                }
-            }           
+            Empleado newEmpleado = db.Empleado.SingleOrDefault(b => b.EmpNo == empleado.EmpNo);
+            newEmpleado.FechaContrato = null;
+            DeptoEmp deptoEmpToUpdate = db.DeptoEmp.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
+            deptoEmpToUpdate.FechaFin = DateTime.Now;
+            Sueldos sueldo = db.Sueldos.SingleOrDefault(b => b.EmpNo == empleado.EmpNo);
+            sueldo.HastaFecha = DateTime.Now;
+            Cargo cargo = db.Cargo.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
+            cargo.FechaFin = DateTime.Now;
+            if (db.DeptoJefe.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo) != null)
+            {
+                DeptoJefe deptoJefe = db.DeptoJefe.SingleOrDefault(b => b.EmpleadoEmpNo == empleado.EmpNo);
+                deptoJefe.FechaFin = DateTime.Now;
+            }
+            db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No pude escribir algo a la bd");
+                return NotFound();
+            }       
             return RedirectToAction(nameof(Index));
         }
 
